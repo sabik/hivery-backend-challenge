15a00c8fc0acc318dce6d3a6203969b0db65b17c
Jiri Baum <jiri@baum.com.au>
2019-02-19 23:59:49 +1100

    Style fixes

--- paranuara/paranuara.py
+++ paranuara/paranuara.py
@@ -11,20 +11,30 @@ DEFAULT_FOODS_JSON = path.join(DATA_PATH_PREFIX, 'foods.json')
 
 
 class Data:
-    def __init__(self, *, companies_json=DEFAULT_COMPANIES_JSON,
-                 people_json=DEFAULT_PEOPLE_JSON, foods_json=DEFAULT_FOODS_JSON):
+    def __init__(self, *,
+                 companies_json=DEFAULT_COMPANIES_JSON,
+                 people_json=DEFAULT_PEOPLE_JSON,
+                 foods_json=DEFAULT_FOODS_JSON):
         self.companies = pandas.read_json('resources/companies.json')
-        self.companies = self.companies.set_index(self.companies['company'], verify_integrity=True)
+        self.companies = self.companies.set_index(self.companies['company'],
+                                                  verify_integrity=True)
 
         self.people = pandas.read_json('resources/people.json')
         # flatten the "friends" dictionary
-        self.people['friends'] = self.people['friends'].map(lambda fs: [f['index'] for f in fs])
+        self.people['friends'] = self.people['friends'].map(
+            lambda fs: [f['index'] for f in fs]
+        )
         # pre-calculate the usernames
-        self.people['username'] = self.people['email'].map(lambda e: e.split('@')[0])
-        self.people = self.people.set_index(self.people['username'], verify_integrity=True)
+        self.people['username'] = self.people['email'].map(
+            lambda e: e.split('@')[0]
+        )
+        self.people = self.people.set_index(self.people['username'],
+                                            verify_integrity=True)
 
         self.foods = pandas.read_json('resources/foods.json')
-        self.foods = self.foods.set_index(self.foods['food'], verify_integrity=True)
+        self.foods = self.foods.set_index(self.foods['food'],
+                                          verify_integrity=True)
+
 
 @lru_cache()
 def get_data():
@@ -44,9 +54,11 @@ def jsonify_pd(obj, **kwargs):
 
 app = Flask(__name__)
 
+
 @app.route("/v1/company/<string:company>/employees")
 def employees(company):
-    return jsonify_pd(_employees(company, data = get_data()), orient='records')
+    return jsonify_pd(_employees(company, data=get_data()), orient='records')
+
 
 def _employees(company, *, data):
 
@@ -61,10 +73,15 @@ def _employees(company, *, data):
 def mutual_info(username1, username2):
     return jsonify(_mutual_info(username1, username2, data=get_data()))
 
+
 def _mutual_info(username1, username2, *, data):
     persons = data.people.loc[[username1, username2]]
     friends1, friends2 = (set(x) for x in persons['friends'])
-    mutuals = data.people.set_index(data.people['index']).loc[list(friends1 & friends2)]
+    mutuals = (
+        data.people
+        .set_index(data.people['index'])
+        .loc[list(friends1 & friends2)]
+    )
     mutuals = mutuals[mutuals['eyeColor'] == 'brown']
     mutuals = mutuals[mutuals['has_died'] == False]
 
@@ -80,6 +97,7 @@ def _mutual_info(username1, username2, *, data):
 @app.route("/v1/person/<string:username>/favourites")
 def favourites(username):
     return jsonify_pd(_favourites(username, data=get_data()))
+
 
 def _favourites(username, *, data):
     person = data.people.loc[username]
